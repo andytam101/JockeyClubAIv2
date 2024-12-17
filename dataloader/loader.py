a44bba8499dff97dc4023f845ec75136cdb5b3db
Andy Tam <andytam321@gmail.com>
2024-12-18 04:52:27 +0800

    refactor: re-implement basic structure of base (abstract) classes of model and dataloader

--- dataloader/loader.py
+++ dataloader/loader.py
@@ -11,13 +11,56 @@ class Loader(ABC):
         self.session = get_session()
         self.save_dir = save_dir
 
+    def load_train(self, cv_size, directory=None):
+        if directory is not None:
+            x, y = self._load_from_dir(directory)
+        else:
+            x, y = self._load_from_db()
+            self.save(x, y)
+
+        self._shuffle(x, y)
+
+        m = x.shape[0]
+        cv_idx = int(m * (1 - cv_size))
+        train_x = x[:cv_idx]
+        train_y = y[:cv_idx]
+        cv_x = x[cv_idx:]
+        cv_y = y[cv_idx:]
+
+        return train_x, train_y, cv_x, cv_y
+
     @abstractmethod
-    def load(self):
-        """
-        Reads data from database and transforms it into useful data for training.
-        :return:
-        """
-        pass
+    def _load_from_dir(self, directory):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _load_from_db(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def load_predict(self, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def train_normalize(self, train_x):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def normalize(self, data, **kwargs):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _shuffle(x, y):
+        mx, nx = x.shape[0], x.shape[1]
+        my, ny = y.shape[0], y.shape[1]
+
+        assert mx == my     # same set of data
+        combined = np.zeros((mx, nx + ny), dtype=np.float32)
+        combined[:, :nx] = x
+        combined[:, nx:] = y
+        np.random.shuffle(combined)
+
+        return combined[:, :nx], combined[:, nx:]
 
     def save(self, data_x, data_y):
         if self.save_dir:
--- model/_model.py
+++ model/_model.py
@@ -1,12 +1,68 @@
+import torch
 from torch import nn
 from abc import ABC, abstractmethod
+import os
 
+from dataloader.loader import Loader as DataLoader
 
 class _Model(nn.Module, ABC):
-    def __init__(self, dataloader):
+    def __init__(self, output_model_dir, data_path=None):
         super(_Model, self).__init__()
-        self.from_load = False
-        self.dataloader = dataloader
+        self.optimizer = None
+        self.data_path = data_path
+        self.normalization = None
+        self.input_model_path  = None
+        self.output_model_path = output_model_dir
+        self.dataloader: DataLoader = self._data_loader()
+
+    def load(self, model_dir=None):
+        """Function must be called, can maybe be integrated into __init__"""
+        self.optimizer = self._optimizer
+        if model_dir is None:
+            return
+
+        model_state_dict_path = os.path.join(model_dir, "model_state.pth")
+        optimizer_state_dict_path = os.path.join(model_dir, "optimizer_state.pth")
+        self.load_state_dict(torch.load(model_state_dict_path))
+        self.optimizer.load_state_dict(torch.load(optimizer_state_dict_path))
+        self.input_model_path = model_dir
+
+        # children classes will do more (with normalisation stuff)
+        self._load_normalization(model_dir)
+
+    def save(self):
+        os.makedirs(self.output_model_path, exist_ok=True)
+        model_state_path = os.path.join(self.output_model_path, "model_state.pth")
+        optimizer_state_dict_path = os.path.join(self.output_model_path, "optimizer_state.pth")
+        torch.save(self.state_dict(), model_state_path)
+        torch.save(self.optimizer.state_dict(), optimizer_state_dict_path)
+
+        self.dataloader.close()
+
+        # children classes will do more (with normalisation stuff)
+        self._save_normalization(self.output_model_path)
+
+    def train_model(self, cv_size, epochs=10000):
+        train_x, train_y, cv_x, cv_y = self.dataloader.load_train(cv_size, directory=self.data_path)
+        self.normalization = self.dataloader.train_normalize(train_x)     # returns **kwargs
+        self.dataloader.normalize(cv_x, **self.normalization)
+
+        # TODO: do actual training
+
+        self.save()
+
+    @property
+    @abstractmethod
+    def _dataloader(self):
+        """
+        Returns basic unloaded type of dataloader required by each model
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def _optimizer(self):
+        raise NotImplementedError
 
     @abstractmethod
     def predict(self, x):
@@ -14,10 +70,6 @@ class _Model(nn.Module, ABC):
 
     @abstractmethod
     def forward(self, x):
-        raise NotImplementedError
-
-    @abstractmethod
-    def optimizer(self):
         raise NotImplementedError
 
     @staticmethod
@@ -29,5 +81,10 @@ class _Model(nn.Module, ABC):
     def accuracy(self, output, target):
         raise NotImplementedError
 
-    def load(self, model_dir):
-        self.from_load = True
+    @abstractmethod
+    def _load_normalization(self, model_directory):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _save_normalization(self, model_directory):
+        raise NotImplementedError
